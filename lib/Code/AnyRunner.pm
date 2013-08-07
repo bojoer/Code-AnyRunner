c98287640349f4526616ff0b27205af1842819d1
Kosuke Asami <tfortress58@gmail.com>
2013-08-07 17:34:31 +0900

    initial commit

---
+++ lib/Code/AnyRunner.pm
@@ -0,0 +1,32 @@
+package Code::AnyRunner;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Code::AnyRunner -
+
+=head1 SYNOPSIS
+
+  use Code::AnyRunner;
+
+=head1 DESCRIPTION
+
+Code::AnyRunner is
+
+=head1 AUTHOR
+
+Kosuke Asami E<lt>tfortress58@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
